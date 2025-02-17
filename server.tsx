113ca27544fe938fce6aa230ae59b8e22f620bb5
Zoran Duric <zordjuric@gmail.com>
2025-02-17 10:40:11 +0200

    Rendering React

--- server.tsx
+++ server.tsx
@@ -1,10 +1,22 @@
-const port = process.env.PORT || 3000
- 
-console.log(`Launching Bun HTTP server on port: ${port}, url: http://0.0.0.0:${port} 🚀`)
- 
+import { renderToReadableStream } from "react-dom/server";
+import { Index } from "./src/Index";
+
+const port = process.env.PORT || 3000;
+
+console.log(
+  `Launching Bun HTTP server on port: ${port}, url: http://0.0.0.0:${port} 🚀`
+);
+
 Bun.serve({
-  port: port,
-  fetch(_request) {
-    return new Response('Hello from Hello via Bun!')
+  async fetch(req) {
+    const { pathname } = new URL(req.url);
+    if (pathname === "/") {
+      const stream = await renderToReadableStream(<Index />);
+      return new Response(stream, {
+        headers: { "Content-Type": "text/html" },
+      });
+    } else {
+      return new Response("Not found", { status: 404 });
+    }
   },
-})
+});
---
+++ src/Index.tsx
@@ -0,0 +1,12 @@
+export function Index() {
+  return (
+    <html>
+      <head>
+        <script src="index.js" defer></script>
+      </head>
+      <body id="root">
+        <p>Hello world</p>
+      </body>
+    </html>
+  );
+}
